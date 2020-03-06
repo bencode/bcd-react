cfea5c856ab16091139d47f781f03a166db112b1
bencode <bencode@163.com>
2020-03-06 21:40:31 +0800

    remove examples

--- examples/webpack/config/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const createConfig = require('bcd-react-webpack');
-
-const env = process.env.NODE_ENV || 'development';
-
-module.exports = createConfig({
-  publicPath: env === 'development' ? '/' :
-    'https://lesspage.oss-cn-shanghai.aliyuncs.com/'
-});
--- examples/webpack/src/pages/index/index.js
+++
@@ -1,6 +0,0 @@
-const body = document.querySelector('body');
-
-const elm = document.createElement('div');
-elm.innerHTML = 'World';
-
-body.appendChild(elm);
