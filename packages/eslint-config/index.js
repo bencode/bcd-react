ffefc37b0fcb8a0a922ddcb539fc3528ba577576
qijun.weiqj <qijun.weiqj@alibaba-inc.com>
2021-12-04 17:15:31 +0800

    bump version v4.0.0

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,7 +1,14 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  plugins: ['jsx-a11y', 'react'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['jsx-a11y', 'react', '@typescript-eslint'],
   extends: [
     require.resolve('eslint-config-airbnb-base'),
     'plugin:react/recommended'
@@ -13,5 +20,14 @@ module.exports = {
     jest: true,
     node: true
   },
-  rules: require('./rules')
+  rules: {
+    ...require('./rules'),
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error']
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 };
